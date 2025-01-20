f1a344cdbd7ed24d77e072d49207b1692f2ebcc0
Austin <austin@austinlong.dev>
2025-01-20 14:03:57 -0500

    Added Motion (previously Framer Motion)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { VStack } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
+import * as motion from "motion/react-client";
 
 export default function Home() {
   return (
@@ -7,6 +8,12 @@ export default function Home() {
       <p className="text-4xl">Hello world</p>
       <Button>Click me</Button>
       <Button variant="outline">ABC</Button>
+      <Box asChild height="40px" width="40px" bg="red.500" rounded={8}>
+        <motion.div whileHover={{ scale: 1.2, rotate: 90 }}></motion.div>
+      </Box>
     </VStack>
   );
 }
+
+// <Box asChild height="40px" width="40px" bg="red.500">
+// </Box>
