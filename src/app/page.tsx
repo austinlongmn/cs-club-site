62340ce428272b8d60dc6ddf608ec4c7c7960f70
Austin <austin@austinlong.dev>
2025-01-20 12:58:26 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { VStack } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <VStack>
+      <Button>Click me</Button>
+      <Button variant="outline">ABC</Button>
+    </VStack>
+  );
+}
