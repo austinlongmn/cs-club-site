2e413277401f64d4ddc0f6c1d5a1825300cffd1e
Austin <austin@austinlong.dev>
2025-01-20 13:08:49 -0500

    Added Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { VStack } from "@chakra-ui/react";
 export default function Home() {
   return (
     <VStack>
+      <p className="text-4xl">Hello world</p>
       <Button>Click me</Button>
       <Button variant="outline">ABC</Button>
     </VStack>
