c76de6fa88a711a6983fe60cb2f62322ba2eb0e6
Austin <austin@austinlong.dev>
2025-01-20 14:18:33 -0500

    Had a lot of fun with Motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, Text, VStack } from "@chakra-ui/react";
 import * as motion from "motion/react-client";
 
 export default function Home() {
@@ -8,8 +8,16 @@ export default function Home() {
       <p className="text-4xl">Hello world</p>
       <Button>Click me</Button>
       <Button variant="outline">ABC</Button>
-      <Box asChild height="40px" width="40px" bg="red.500" rounded={8}>
-        <motion.div whileHover={{ scale: 1.2, rotate: 90 }}></motion.div>
+      <Box asChild bg="red.500" width={200} height={200} padding={10} rounded={8}>
+        <motion.div
+          whileHover={{
+            scale: 10,
+            rotate: 360,
+            backgroundColor: "rgb(0,100,0)",
+            transition: { duration: 0.5 }
+          }}>
+          <Text>Hover me and see what happens...</Text>
+          </motion.div>
       </Box>
     </VStack>
   );
