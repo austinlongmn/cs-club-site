a9db56f6d37c825f508d42168fb5c37b06a58d56
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-01-31 19:09:43 -0500

    Bootstrapping good stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
-import { Button } from "@/components/ui/button";
-import { Box, Text, VStack } from "@chakra-ui/react";
-import * as motion from "motion/react-client";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <VStack>
-      <p className="text-4xl">Hello world</p>
-      <Button>Click me</Button>
-      <Button variant="outline">ABC</Button>
-      <Box asChild bg="red.500" width={200} height={200} padding={10} rounded={8}>
-        <motion.div
-          whileHover={{
-            scale: 10,
-            rotate: 360,
-            backgroundColor: "rgb(0,100,0)",
-            transition: { duration: 0.5 }
-          }}>
-          <Text>Hover me and see what happens...</Text>
-          </motion.div>
-      </Box>
-    </VStack>
+    <Box padding="10">
+      <Heading className="text-6xl pt-10 pb-5">Welcome to the CS Club Site!</Heading>
+      <Text>Cool stuff coming your way soon!</Text>
+    </Box>
   );
 }
-
-// <Box asChild height="40px" width="40px" bg="red.500">
-// </Box>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+import { Switch, Flex, Button, IconButton } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+export default function Navbar() {
+  return (
+    <></>
+  );
+}
