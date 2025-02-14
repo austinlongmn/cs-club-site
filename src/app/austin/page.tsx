8c4aa95e3f6b0c2e978dbb8b91b150bb9ebb6446
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-02-14 16:19:02 -0500

    Added placeholder stuff

---
+++ src/app/austin/page.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+export default function Page() {
+  return (
+    <>
+      <Heading>This is Austin&apos;s Route!</Heading>
+      <Text>We are working on it.</Text>
+    </>
+  )
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Heading } from "@chakra-ui/react";
+
 export default function Navbar() {
   return (
-    <></>
+    <div className="bg-gray-800 text-white">
+      <nav>
+        <Heading className="text-center p-5">There will be a navigation bar here.</Heading>
+      </nav>
+    </div>
   );
 }
