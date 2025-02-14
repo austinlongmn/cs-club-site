8254f53bd4bdb8d1050a00ff8de2c697c863732a
Breyon Gunn <40876246+SuperBGLazer@users.noreply.github.com>
2025-02-14 16:24:50 -0600

    Work on adding my stuff

---
+++ src/app/breyon/page.tsx
@@ -0,0 +1,19 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Box padding="10">
+      <Heading className="text-6xl pt-10 pb-5">Breyon Gunn</Heading>
+      
+      <Text>
+        I’m a software developer and tech enthusiast with a passion for problem-solving, cybersecurity, and competitive programming. With years of experience in software engineering, database management, and system architecture, I enjoy creating innovative solutions and tackling complex challenges.
+      </Text>
+      <Text>
+        When I’m not writing code, you’ll find me organizing programming competitions, mentoring students in computer science, or diving into the latest advancements in technology. I also have a deep interest in ethical hacking, automation, and infrastructure optimization, always looking for new ways to push the limits of what’s possible.
+      </Text>
+      <Text>
+        In addition to software development, I enjoy go-kart racing, constantly chasing the perfect lap and refining my driving technique. I have also developed programs for AKC agility scoring, combining my love for technology with the precision required in competition settings.
+      </Text>
+    </Box>
+  );
+}
