d737868f3796c0b0bcc97527fed5496a42aa8bec
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-02-20 21:02:33 -0500

    Added projects.tsx

---
+++ src/app/austin/projects/page.tsx
@@ -0,0 +1,41 @@
+import { Text } from "@chakra-ui/react";
+import { Key } from "react";
+
+interface ProjectPreviewProps {
+  project: Project
+  listKey: Key | null | undefined
+}
+
+function ProjectPreview(props: ProjectPreviewProps) {
+  const project = props.project
+  return (
+    <div key={props.listKey}>
+      <Text>{project.title}</Text>
+      <Text>{project.description}</Text>
+    </div>
+  )
+}
+
+interface Project {
+  title: string;
+  description: string;
+}
+
+export default function Projects() {
+  const projects: Project[] = [
+    { title: "My First project", description: "First project description" },
+    { title: "My Second project", description: "Second project description" }
+  ]
+
+  return (
+    <>
+      {
+        projects.map((project, index) => {
+          return (
+            <ProjectPreview project={project} listKey={index} />
+          )
+        })
+      }
+    </>
+  )
+}
