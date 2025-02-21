85fe299044030ca38b7d044ca0ae3c440393f371
JosieMellick <mellick1110@gmail.com>
2025-02-20 19:59:09 -0600

    did stuff

---
+++ src/app/josie/page.tsx
@@ -0,0 +1,18 @@
+import { Heading, Text } from "@chakra-ui/react"
+import { Project } from "next/dist/build/swc/types"
+import { Key } from "react";
+
+export default function Page() {
+    return(
+        <>
+        <Heading>Josie Mellick</Heading>
+        <Text>My name is Josie Mellick and I have a passion for coding.
+            I'm a sophmore computer science student at UNW.
+            I'm profient in Python, Java, and I'm currently learning C++.
+            My main interest is in video game programming,
+            but I'm also interested in sofware development.
+        </Text>
+        </>
+    )
+}
+
---
+++ src/app/josie/projects/page.tsx
@@ -0,0 +1,41 @@
+import { Heading, Text } from "@chakra-ui/react"
+import { Key } from "react";
+
+interface ProjectPreviewProps {
+    project: Project
+    list_key: Key | null | undefined
+}
+
+function ProjectPreview(props: ProjectPreviewProps) {
+    const project = props.project
+    return (
+        <div key={props.list_key}>
+            <Text>{project.title}</Text>
+            <Text>{project.description}</Text>
+        </div>
+    )
+}
+
+interface Project {
+    title: string;
+    description: string;
+}
+
+export default function Projects() {
+    const projects: Project[] = [
+        {title: "My first project", description: "First project description"},
+        {title: "My second project", description: "Second project description"}
+    ]
+    
+    return (
+        <>
+            {
+                projects.map((project, index) => {
+                    return (
+                        <ProjectPreview project={project} list_key={index} />
+                    )
+                })
+            }
+        </>
+    )
+}
