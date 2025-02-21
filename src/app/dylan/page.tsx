b573b4dbcd0d2e31da0c37eca584b6d22fe4a1d4
Dylan Weakly <dcweakly@students.unwsp.edu>
2025-02-20 18:35:54 -0600

    Add Dylan Page

---
+++ src/app/dylan/page.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Box, Button, Heading, Text, VStack, Container, Flex, Image } from "@chakra-ui/react";
+import { RiMailLine } from "react-icons/ri";
+
+export const metadata: Metadata = {
+    title: "UNW CS Club - Dylan Weakly",
+    description:
+        "Dylan Weakly's site for the UNWSP CS Club"
+    };
+
+export default function Dylan() {
+    return ( 
+        <Container bg="black">
+            <Flex justifyContent="center" alignItems="center">
+                <Heading size="3xl" color="white" py="5">Dylan Weakly</Heading>
+            </Flex>
+            <Box p={10} maxW="600px" mx="auto" bg="white" borderWidth="3px" borderColor="#16C2FF" shadow="md" rounded="3xl">
+            <VStack align="start">
+                
+                <Button variant="surface" color="white" bg="black">
+                        <RiMailLine />
+                        Email Dylan
+                        </Button>
+                        
+                <Text fontSize="md" color="black" py="2">
+                    I am currently a sophomore Computer Science major with a Cybersecurity minor. 
+                    I am currently learning web development with basic HTML, CSS, and JavaScript as well as learning the React framework.
+                </Text>
+                <Text fontSize="md" color="black" py="0" textAlign="right">
+                    Some of my interests outside of Computer Science include going on hikes with my family (yeah, shocking!), checking out cool cars, and serving on the production team at my church.
+                </Text>
+                <Text fontSize="md" color="black" py="5">
+                    For Systems Analysis and Design class here at Northwestern, I worked together with Isaac Grosser who has since graduated to provide Atkinson Refinishing Services with some great invoicing options to improve efficiency of the company.
+                </Text>
+                
+                <Image src="src/app/dylan/assets/atkinson-poster.png" alt="Systems A & D Photo"/>
+
+            </VStack>
+            </Box>
+        </Container>
+        
+    );
+}
