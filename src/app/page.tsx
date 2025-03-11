d94aa94a3335b877931cda3a354f98d2de6a2779
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-11 16:24:56 -0400

    Testing CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <Heading className="text-6xl pt-10 pb-5">Welcome to the CS Club Site!</Heading>
       <Text>Cool stuff coming your way soon!</Text>
+      <Text>Hello, World!</Text>
     </Box>
   );
 }
