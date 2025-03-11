13ff919221d812893d3b68d2321cd0274d7cef66
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-11 14:42:06 -0400

    Added caching

--- .github/scripts/deploy.bash
+++ .github/scripts/deploy.bash
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -e
-echo "Installing dependencies..."
-npm install
-echo "Building application..."
-npm run build
-
 app_dir="$(mktemp -d)/app-staging"
 mkdir $app_dir
 
 identity_file=$(mktemp)
 chmod 600 "$identity_file"
+
 echo "$SSH_PRIVATE_KEY" > $identity_file
+printf '%s\n' "$(cat $identity_file | tr -s '\n')" > $identity_file
 
 function delete_tmp_file {
 	rm $identity_file
 	rm -rf $app_dir
 }
 trap delete_tmp_file EXIT
+
+if [[ ! "`cat $identity_file`" ]]; then
+	echo "Empty private key"
+	exit 1
+else
+	echo "Private key is in-place."
+fi
 
 cp -r \
 	.next \
@@ -28,10 +32,11 @@ cp -r \
 	$app_dir
 
 function remote_cmd {
-	ssh -T -p $WEBSERVER_SSH_PORT -i "$identity_file" $WEBSERVER_SSH_USER@$WEBSERVER_SSH_HOST $@ | \
+	ssh -T -p $WEBSERVER_SSH_PORT -i "$identity_file" -o IdentitiesOnly=yes -o BatchMode=yes $WEBSERVER_SSH_USER@$WEBSERVER_SSH_HOST $@ | \
 		awk '{print "\033[36mRemote: " $0 "\033[0m"}'
 }
 
+echo "Connecting to remote..."
 remote_cmd << EOF
 	set -e
 	echo "Getting ready to receive..."
@@ -48,8 +53,9 @@ remote_cmd << EOF
 EOF
 
 echo "Copying app to remote..."
-rsync -r -e "ssh -p $WEBSERVER_SSH_PORT -i \"$identity_file\"" --del $app_dir $WEBSERVER_SSH_USER@$WEBSERVER_SSH_HOST:~
+rsync -r -e "ssh -p $WEBSERVER_SSH_PORT -i \"$identity_file\" -o IdentitiesOnly=yes -o BatchMode=yes" --del $app_dir $WEBSERVER_SSH_USER@$WEBSERVER_SSH_HOST:~
 
+echo "Connecting to remote..."
 remote_cmd << EOF
 	set -e
 	echo "Backing up old app..."
