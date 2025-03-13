e22210e6451a95b01f9318ab00f0c3815026d9d5
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-13 16:57:47 -0400

    Fix formatting

--- src/app/austin/projects/page.tsx
+++ src/app/austin/projects/page.tsx
@@ -2,19 +2,19 @@ import { Text } from "@chakra-ui/react";
 import { Key } from "react";
 
 interface ProjectPreviewProps {
-  project: Project
-  listKey: Key | null | undefined
+  project: Project;
+  listKey: Key | null | undefined;
 }
 
 function ProjectPreview(props: ProjectPreviewProps) {
-  const project = props.project
+  const project = props.project;
   return (
     <div key={props.listKey}>
       <Text>The key is {props.listKey}</Text>
       <Text>{project.title}</Text>
       <Text>{project.description}</Text>
     </div>
-  )
+  );
 }
 
 interface Project {
@@ -25,18 +25,14 @@ interface Project {
 export default function Projects() {
   const projects: Project[] = [
     { title: "My First project", description: "First project description" },
-    { title: "My Second project", description: "Second project description" }
-  ]
+    { title: "My Second project", description: "Second project description" },
+  ];
 
   return (
     <>
-      {
-        projects.map((project, index) => {
-          return (
-            <ProjectPreview project={project} listKey={index} />
-          )
-        })
-      }
+      {projects.map((project, index) => {
+        return <ProjectPreview project={project} listKey={index} />;
+      })}
     </>
-  )
+  );
 }
