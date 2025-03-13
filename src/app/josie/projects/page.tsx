2926f95ecf635393525cbeb4c303e67eea2a57a8
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-13 19:02:40 -0400

    Fix warnings

--- src/app/josie/projects/page.tsx
+++ src/app/josie/projects/page.tsx
@@ -1,41 +1,37 @@
-import { Heading, Text } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react";
 import { Key } from "react";
 
 interface ProjectPreviewProps {
-    project: Project
-    list_key: Key | null | undefined
+  project: Project;
+  key: Key | null | undefined;
 }
 
 function ProjectPreview(props: ProjectPreviewProps) {
-    const project = props.project
-    return (
-        <div key={props.list_key}>
-            <Text>{project.title}</Text>
-            <Text>{project.description}</Text>
-        </div>
-    )
+  const project = props.project;
+  return (
+    <div>
+      <Text>{project.title}</Text>
+      <Text>{project.description}</Text>
+    </div>
+  );
 }
 
 interface Project {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 
 export default function Projects() {
-    const projects: Project[] = [
-        {title: "My first project", description: "First project description"},
-        {title: "My second project", description: "Second project description"}
-    ]
-    
-    return (
-        <>
-            {
-                projects.map((project, index) => {
-                    return (
-                        <ProjectPreview project={project} list_key={index} />
-                    )
-                })
-            }
-        </>
-    )
+  const projects: Project[] = [
+    { title: "My first project", description: "First project description" },
+    { title: "My second project", description: "Second project description" },
+  ];
+
+  return (
+    <>
+      {projects.map((project, index) => {
+        return <ProjectPreview project={project} key={index} />;
+      })}
+    </>
+  );
 }
