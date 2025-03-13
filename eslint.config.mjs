3d836240dd5bce1100be72085ebf781ddd4df319
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-12 22:42:53 -0400

    Added filename enforcement!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import onlyWarn from "eslint-plugin-only-warn";
 import unusedImports from "eslint-plugin-unused-imports";
+import checkFile from "eslint-plugin-check-file";
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -33,6 +34,28 @@ const eslintConfig = [
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_",
           caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "check-file/filename-naming-convention": [
+        "warn",
+        {
+          "**/*.{tsx}": "PASCAL_CASE",
+          "**/*.{ts}": "CAMEL_CASE",
+          "src/app/**/*.*": "KEBAB_CASE",
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "warn",
+        {
+          "**/*": "KEBAB_CASE",
+          "src/app/**/*": "NEXT_JS_APP_ROUTER_CASE",
         },
       ],
     },
