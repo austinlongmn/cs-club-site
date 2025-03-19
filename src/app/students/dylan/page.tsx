6e5ba8f1a456bb1e81e2de0ab48be12dac49049a
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-19 09:44:40 -0400

    Fix formatting

--- src/app/students/dylan/page.tsx
+++ src/app/students/dylan/page.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 export default function Dylan() {
   return (
     <div className="flex flex-col items-center justify-center transition-all duration-500">
-      <div className="flex justify-between w-full p-5">
+      <div className="flex w-full justify-between p-5">
         <h1 className="text-5xl text-white">Dylan Weakly</h1>
       </div>
 
-      <div className="p-10 max-w-3xl mx-auto backdrop-blur-lg border-5 border-[#16C2FF] shadow-xl rounded-3xl text-white">
+      <div className="mx-auto max-w-3xl rounded-3xl border-5 border-[#16C2FF] p-10 text-white shadow-xl backdrop-blur-lg">
         <div className="flex flex-col items-center">
-          <button className="flex items-center gap-2 bg-blue-500 text-white px-6 py-3 rounded-lg text-2xl hover:bg-blue-500">
+          <button className="flex items-center gap-2 rounded-lg bg-blue-500 px-6 py-3 text-2xl text-white hover:bg-blue-500">
             <RiMailLine />
             Email Dylan
           </button>
-          <p className="text-sm text-center mt-2">
+          <p className="mt-2 text-center text-sm">
             Developer Website Coming Soon
           </p>
 
@@ -38,14 +38,14 @@ export default function Dylan() {
             improve efficiency of the company.
           </p>
 
-          <h2 className="text-lg py-4 text-center">My Favorite Projects</h2>
-          <ul className="list-disc list-inside">
+          <h2 className="py-4 text-center text-lg">My Favorite Projects</h2>
+          <ul className="list-inside list-disc">
             <li>Systems Analysis and Design Project (featured Below)</li>
             <li>More to come...</li>
           </ul>
 
           <Image
-            className="rounded-lg shadow-md transition-transform duration-300 hover:scale-105 mt-10"
+            className="mt-10 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
             width={1300}
             height={1000}
             src="/images/dylan/atkinson-poster.png"
