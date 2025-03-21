8922acf84931b3559445d36cc0abdf63c76bd94c
JP <josiahp13579@gmail.com>
2025-03-21 16:07:55 -0500

    fixed formmating

--- src/app/students/josiah-paul/page.tsx
+++ src/app/students/josiah-paul/page.tsx
@@ -1,11 +1,8 @@
-
-
-export default function Josiah(){
-    return(
-        <div>
-            <h1 className ="m-20 text-center">Josiah Paul</h1>
-            <p>pluh</p>
-        </div>
-        
-    );
+export default function Josiah() {
+  return (
+    <div>
+      <h1 className="m-20 text-center">Josiah Paul</h1>
+      <p>pluh</p>
+    </div>
+  );
 }
