9e9baa6ae9fd4f8941ed6d7a37109d7277bf79ae
andytheroo13 <31022237+andytheroo13@users.noreply.github.com>
2025-03-21 16:05:29 -0500

    Adding some changes to my site

--- src/app/students/andrew-hayes/page.tsx
+++ src/app/students/andrew-hayes/page.tsx
@@ -10,6 +10,8 @@ export default function Page() {
             margin-right: auto;
             width: 50%;
             }
+    .text {
+            color: red;}
     `}</style>
       <h1>This is Andrew&apos;s Route!</h1>
       <p className="center">Welcome to this empty page.</p>
@@ -19,9 +21,41 @@ export default function Page() {
           style={{ color: "blue" }}
         >
           {" "}
-          Funny video I enjoy
+         Click text for a funny video I enjoy
         </Link>
+        <p>Here are some projects Im working on.</p>
+
+        <li>
+          The Last Crewman 
+
+        </li>
+        <li>
+          Image sorter for personal use
+        </li>
+
+
+
       </section>
+      <section id="about me">
+        <body>
+
+        <p> I am a software developer I also enjoy certain video games</p>
+        <p className="text"> I am a fan of the following games:</p>
+        <li>Fallout 4</li>
+        <li>Minecraft</li>
+        <li>Slay the Princess</li>
+        <li>HollowKnight</li>
+        <p className="text">
+          I am also a fan of the following shows:
+        </p >
+        <li>Star Trek</li>
+        <li>Star Wars</li>
+        <li>Doctor Who</li>
+        <li>Goblin Slayer</li>
+        <li>Friern: Beyond Journey's End</li>
+        </body>
+        </section>
+  
     </>
   );
 }
