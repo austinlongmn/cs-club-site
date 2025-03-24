280d8892a1d5d1f308a00109af02f81fe276f305
Carter Leitch <csleitch@students.unwsp.edu>
2025-03-24 15:03:39 -0500

    fresh start

--- src/app/students/carter-leitch/page.tsx
+++ src/app/students/carter-leitch/page.tsx
@@ -1,27 +1,12 @@
-import Link from "next/link";
+const me = {
+  name: "Carter Leitch",
+}
 
-export default function Page() {
-  return (
+export default function Page(){
+  return(
     <>
-      <style>{`
-    .center {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 50%;
-            }
-    `}</style>
-      <h1>This is Carter&apos;s Route!</h1>
-      <p className="center">Welcome to this empty page.</p>
-      <section id="youtube">
-        <Link
-          href="https://www.youtube.com/watch?v=C12rXla0uuo&pp=ygUOY2xvbmUgd2FycyBzZm0%3D"
-          style={{ color: "blue" }}
-        >
-          {" "}
-          Funny video I enjoy
-        </Link>
-      </section>
+      <h1 style={{ color: "red" }}>{me.name}</h1>
+      <p></p>
     </>
   );
 }
