1c5a57c4dd8c81ec800a7cfa3f56eae936d9e860
Dylan Weakly <dcweakly@students.unwsp.edu>
2025-03-24 21:04:36 -0500

    More Mobile Fixes

--- src/app/students/dylan-weakly/page.tsx
+++ src/app/students/dylan-weakly/page.tsx
@@ -12,23 +12,24 @@ export default function Dylan() {
   return (
     <div className="mx-auto mt-10 flex max-w-4xl flex-col items-center justify-center rounded-4xl border-8 border-[#428ad1] p-10 text-white shadow-xl backdrop-blur-lg transition-all duration-500">
       <div className="flex w-full items-center justify-center px-10">
-        <div className="flex items-center gap-4">
+        <div className="group relative items-center">
           <Image
-            className="rounded-full"
+            className="rounded-full transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:shadow-[0px_0px_20px_5px_rgba(255, 255, 255, 0.8)]"
             src="/images/dylan/dylan.jpg"
-            width={150}
+            width={200}
             height={200}
             alt="Dylan Weakly"
           />
-          <div className="flex flex-col">
-            <h2 className="text-4xl font-bold">Dylan Weakly</h2>
-            <h3 className="mt-5 text-xl">Sophomore at UNW</h3>
-            <p className="mt-2 text-sm">Personal Website Coming Soon...</p>
-          </div>
         </div>
       </div>
+      <div className="flex flex-col mt-10 md:mt-5 items-center gap-2">
+        <h1 className="text-4xl">Dylan Weakly</h1>
+        <h3 className="text-3xl">Sophomore at UNW</h3>
+      </div>
 
-      <p className="mt-10 text-md text-center">
+      <p className="pt-5 md:pt-5 text-sm font-bold">Personal Website Coming Soon...</p>
+
+      <p className="mt-10 text-center text-lg">
         I am a sophomore Computer Science student with a Cybersecurity minor. I
         enjoy programming with Python, TypeScript, and also with the React web
         framework, which is being used to make this website. I am currently
@@ -37,16 +38,16 @@ export default function Dylan() {
         thinking skills while navigating challenges that may arise.
       </p>
 
-      <p className="text-md py-3 text-center">
+      <p className="py-3 pb-10 text-center text-lg">
         Some of my interests outside of Computer Science include going hiking
         with my family at state parks and local parks, checking out cars at car
         shows, and serving on the production team at church.
       </p>
-      <div className="flex w-full flex-col md:flex-row items-center justify-evenly pt-10">
+      <div className="flex w-full flex-col items-center justify-evenly pt-10 pb-10 md:flex-row border-t border-[#428ad1]">
         <h2 className="max-w-40 py-4 text-center text-3xl font-bold">
           Classes I have Taken at UNW
         </h2>
-        <ul className="list-inside list-disc text-center">
+        <ul className="list-inside list-disc text-center text-lg">
           <li>Principles of Computing</li>
           <li>Python Programming</li>
           <li>Systems Analysis and Design</li>
@@ -58,18 +59,18 @@ export default function Dylan() {
         </ul>
       </div>
 
-      <div className="flex w-full flex-col md:flex-row items-center justify-evenly pt-10">
+      <div className="flex w-full flex-col items-center justify-evenly pt-10 pb-10 md:pb-0 md:flex-row border-t border-[#428ad1]">
         <h2 className="max-w-30 py-4 text-center text-3xl font-bold">
           My Favorite Projects
         </h2>
-        <ul className="max-w-78 list-inside list-disc text-center">
+        <ul className="max-w-85 list-inside list-disc text-center text-lg">
           <li>Systems Analysis and Design Project (featured Below)</li>
-          <li>More to come...</li>
+          <li>Halloween Trick or Treating Tracker with SQLite and Tkinter in Python</li>
         </ul>
       </div>
 
       <Image
-        className="mt-10 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
+        className="mt-5 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
         width={400}
         height={1000}
         src="/images/dylan/atkinson-poster.png"
