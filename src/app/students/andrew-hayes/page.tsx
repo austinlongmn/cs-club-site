27dfe4a1b0de05615cd72743c40b2762215a05d3
andytheroo13 <31022237+andytheroo13@users.noreply.github.com>
2025-03-27 16:39:29 -0500

    updated the code to match other pages

--- src/app/students/andrew-hayes/page.tsx
+++ src/app/students/andrew-hayes/page.tsx
@@ -1,74 +1,53 @@
 import Link from "next/link";
-import type { Metadata } from "next";
-// have an issue with Style blocking the next function not sure how to fix it. I had to force the background color to be black 
-// I'm not sure what do to about it.
-export default function Page(bgcolor="black") {
+
+export default function Page() {
   return (
-    
-    <>
-      <style>{`
-    .center {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 50%;
-            }
-    .text {
-            color: red;}
-    .border_style {
-            border-style: solid;
-            border-color: orange;
-            border-width: 5px;
-            padding: 10px;
-            margin: 10px;
-            }
-    `}</style>
-      <h1>This is Andrew&apos;s Route!</h1>
-      <p className="center">Welcome to this empty page.</p>
-      <section id="youtube">
+    <div className="min-h-screen bg-black p-6 text-white">
+      <h1 className="mb-4 text-center text-3xl font-bold">
+        This is Andrew&apos;s Route!
+      </h1>
+      <p className="mb-6 text-center">Welcome to this empty page.</p>
+
+      <section className="mb-6">
         <Link
           href="https://www.youtube.com/watch?v=C12rXla0uuo&pp=ygUOY2xvbmUgd2FycyBzZm0%3D"
-          style={{ color: "blue" }}
+          className="block text-center text-blue-500 hover:underline"
         >
-          {" "}
-         Click text for a funny video I enjoy
+          Click text for a funny video I enjoy
         </Link>
-        <p>Here are some projects Im working on.</p>
+        <p className="mt-4 text-center">
+          Here are some projects I'm working on:
+        </p>
+        <ul className="list-inside list-disc text-center">
+          <li>The Last Crewman</li>
+          <li>Image sorter for personal use</li>
+        </ul>
+      </section>
 
-        <li>
-          The Last Crewman 
-
-        </li>
-        <li>
-          Image sorter for personal use
-        </li>
-
-
-
-      </section >
-      <section id="about-me" className="border_style">
-        <div style={{ textAlign: "center" }}>
-          <p> I am a software developer. I also enjoy certain video games.</p>
-          <p className="text"> I am a fan of the following games:</p>
-          <ul>
-        <li>Fallout 4</li>
-        <li>Minecraft</li>
-        <li>Slay the Princess</li>
-        <li>HollowKnight</li>
-          </ul>
-          <p className="text">
-        I am also a fan of the following shows:
+      <section className="rounded-lg border-4 border-orange-500 p-4">
+        <div className="text-center">
+          <p>I am a software developer. I also enjoy certain video games.</p>
+          <p className="mt-2 text-red-500">
+            I am a fan of the following games:
           </p>
-          <ul>
-        <li>Star Trek</li>
-        <li>Star Wars</li>
-        <li>Doctor Who</li>
-        <li>Goblin Slayer</li>
-        <li>Friern: Beyond Journey's End</li>
+          <ul className="list-inside list-disc">
+            <li>Fallout 4</li>
+            <li>Minecraft</li>
+            <li>Slay the Princess</li>
+            <li>Hollow Knight</li>
+          </ul>
+          <p className="mt-4 text-red-500">
+            I am also a fan of the following shows:
+          </p>
+          <ul className="list-inside list-disc">
+            <li>Star Trek</li>
+            <li>Star Wars</li>
+            <li>Doctor Who</li>
+            <li>Goblin Slayer</li>
+            <li>Frieren: Beyond Journey's End</li>
           </ul>
         </div>
       </section>
-  
-    </>
+    </div>
   );
 }
