ab149d366bf47243fdadd9c872d5117134911d83
Dylan Weakly <dcweakly@students.unwsp.edu>
2025-03-27 11:51:47 -0500

    Add Basic Alumni Page

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -1,5 +1,7 @@
-import { div } from "motion/react-client";
 import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CSCLogo from "@/svg/csc-logo.svg";
 
 export const metadata: Metadata = {
   title: "Alumni - UNW CS Club",
@@ -9,9 +11,29 @@ export const metadata: Metadata = {
 };
 
 export default function Alumni() {
+  const paragraphStyle = "text-3xl text-center";
+
   return (
-    <div>
-      <p>Alumni Route</p>
+    <div className="flex flex-col items-center justify-center gap-5 bg-gray-900 pt-20 md:pt-30">
+      <p className={paragraphStyle}>
+        There are currently no active alumni pages.
+      </p>
+      <p className={paragraphStyle}>
+        Check back in Summer 2025 to see the most recent Alumni.
+      </p>
+      <Link
+        href="/students"
+        className="text-center text-2xl underline decoration-[#d0623b]"
+      >
+        Instead, check out our current students!
+      </Link>
+      <Image
+        src={CSCLogo}
+        alt="CSC Logo"
+        width={800}
+        height={600}
+        className="mt-10"
+      />
     </div>
-  )
+  );
 }
