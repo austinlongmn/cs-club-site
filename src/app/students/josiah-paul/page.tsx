645628f71bd1f8fde80f554510e6bb376241a6be
JP <josiahp13579@gmail.com>
2025-03-28 16:32:06 -0500

    Added an about section

--- src/app/students/josiah-paul/page.tsx
+++ src/app/students/josiah-paul/page.tsx
@@ -4,7 +4,31 @@ export default function Josiah() {
       <h1 className="m-10 text-center text-[60px]">
         <b>Josiah Paul</b>
       </h1>
-      <p>pluh</p>
+      <p className="text-center text-[30px]">
+        Computer Science student and CSC member!
+      </p>
+      <div id="about" className="px-180 pt-30 pl-1 text-[25px]">
+        <p className="pl-37 font-bold">About</p>
+        <p>
+          {" "}
+          &nbsp;&nbsp;&nbsp;&nbsp;I have been a CSC member since 2024 and a Comp
+          Sci student since 2022.
+          <br></br>So far, I would say python has been the most fun language to
+          learn.
+        </p>
+        <a href="https://github.com/JP-STHS" target="_blank">
+          <img
+            id="github-logo"
+            className="pt-6 invert-100"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/960px-Octicons-mark-github.svg.png"
+            width={100}
+          />
+        </a>
+      </div>
+
+      {/* Java Script in React */}
+      <script></script>
     </div>
   );
 }
+// explain a bit about urself, projects u worked on with club, projects u worked on in general
