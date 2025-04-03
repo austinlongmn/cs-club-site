e3147ed01aae856b9f0cf635887f4e110d7fad7d
JosieMellick <mellick1110@gmail.com>
2025-04-03 09:18:02 -0500

    Image and Name side by side

--- src/app/students/josie-mellick/page.tsx
+++ src/app/students/josie-mellick/page.tsx
@@ -2,30 +2,41 @@ import Image from 'next/image';
 import Link from "next/link";
 
 
+
+
 export default function Page() {
   return (
     <div>
       {/* Fancy formatting stuff */}
       <div className="flex flex-col items-center justify-center">
 
-        {/* Name and (eventually) picture */}
-        <h1 className="text-center text-6xl text-white">Josie Mellick</h1>
+      <div style={{ height: 40, width: 1 }}></div>
 
-        <Link href="mailto:mellick1110@gmail.com" className="text-blue-400 hover:underline">
-          mellick1110@gmail.com
-        </Link>
+        {/* Allows image and text to be side by side */}
+        <div className="flex items-center space-x-6">
+          {/* Headshot */}
+          <Image className="rounded-lg"
+              width={300}
+              height={300}
+              src="/images/josie/Headshot.png"
+              alt="Mellick Headshot"
+          />
 
-        <div style={{ height: 30, width: 1 }}></div>
+          {/* Name and email */}
+          <div className="text-center">
+            <h1 className="text-7xl text-white">Josie Mellick</h1>
+            <p>University of Northwestern - St. Paul</p>
+            <Link href="mailto:mellick1110@gmail.com" className="text-blue-400 hover:underline">
+              mellick1110@gmail.com
+            </Link>
+            {' ------ '}
+            <Link href="https://www.linkedin.com/in/josie-mellick-60b430327?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BXxqV4ocSQjyo4ZAktwkKvQ%3D%3D" className="text-blue-400 hover:underline">
+              LinkedIn
+            </Link>
+          </div>
+        </div>
 
-        {/* Headshot */}
-        <Image className="rounded-lg"
-            width={300}
-            height={300}
-            src="/images/josie/Headshot.png"
-            alt="Mellick Headshot"
-        />
- 
-        <div style={{ height: 30, width: 1 }}></div>
+        <div style={{ height: 40, width: 1 }}></div>
 
         {/* Introduction and bio */}
         <div className="max-w-2xl mx-auto p-4 bg-gray-900 rounded-lg shadow">
@@ -33,17 +44,17 @@ export default function Page() {
               About Me
             </h2>
           <p className="text-md py-4 text-left">
-            My name is Josie Mellick and I have a passion for programming. I'm 
-            currently a sophomore computer science student at UNW. My experience is 
+            My name is Josie Mellick and I have a passion for programming. I'm
+            currently a sophomore computer science student at UNW. My experience is
             primarily with Python, Java, and C++.
           </p>
-          <p className="text-md py-4 text-left"> 
-            My main interest is in video game programming. I am currently learning C++ 
-            and Unreal Engine to get familiar with how games are made, and I look forward 
+          <p className="text-md py-4 text-left">
+            My main interest is in video game programming. I am currently learning C++
+            and Unreal Engine to get familiar with how games are made, and I look forward
             to growing in this area.
           </p>
-          <p className="text-md py-4 text-left"> 
-            Outside of computer science, I enjoy playing the guitar and I'm on 
+          <p className="text-md py-4 text-left">
+            Outside of computer science, I enjoy playing the guitar and I'm on
             Northwestern's flag football team.
           </p>
         </div>
@@ -53,25 +64,31 @@ export default function Page() {
           Projects
         </h1>
 
-        <div className="flex flex-col items-center justify-center max-w-2xl mx-auto p-4 bg-gray-900 rounded-lg shadow">
+
+
+        <div className="max-w-2xl mx-auto p-4 bg-gray-900 rounded-lg shadow">
           {/* Systems A & D Jack Links project */}
           <h2 className="py-4 text-left text-2xl text-white">
             Offloading Automation for Jack Links
           </h2>
           <p className="text-md py-4 text-left text-white">
-            A partner and I designed and created a script to automate the multi-step 
-            offboarding process. We designed it to work with the pre-existing Microsoft 
-            Entra system. This project was completed for UNW's Systems Analysis & Design 
+            A partner and I designed and created a script to automate the multi-step
+            offboarding process. We designed it to work with the pre-existing Microsoft
+            Entra system. This project was completed for UNW's Systems Analysis & Design
             class. The poster we made for this project is shown below:
           </p>
 
+          <div style={{ height: 20, width: 1 }}></div>
+
+          <div className = "flex flex-col items-center justify-center">
           {/* Systems A & D poster */}
-          <Image className="rounded-lg"
-            width={520}
-            height={400}
-            src="/images/josie/JackLinksProject.png"
-            alt="Systems A&D Poster"
-          />
+            <Image className="rounded-lg"
+              width={520}
+              height={400}
+              src="/images/josie/JackLinksProject.png"
+              alt="Systems A&D Poster"
+            />
+          </div>
 
           <div style={{ height: 30, width: 1 }}></div>
 
