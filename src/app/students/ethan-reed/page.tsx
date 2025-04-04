599b42be5804600c512a634c29c516415dd5f13e
EWARS2 <jefmacho99@gmail.com>
2025-04-04 15:54:56 -0500

    Create from Austin

---
+++ src/app/students/ethan-reed/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <h1>Ethan Reed</h1>
+      <p>I make things and stuff.</p>
+	  <p>This page is a WIP!</p>
+	  
+    </>
+  );
+}
