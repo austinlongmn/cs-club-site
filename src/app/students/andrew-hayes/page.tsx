74d9ed5ed0c358cd5b9bd93f69d3156c1c010247
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-04-11 17:12:57 -0400

    Fix issues

--- src/app/students/andrew-hayes/page.tsx
+++ src/app/students/andrew-hayes/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
       </div>
       <div className="flex justify-center">
         {/* GitHub logo button */}
-        <a
+        <Link
           href="https://github.com/andytheroo13"
           target="_blank"
           rel="noopener noreferrer"
@@ -34,7 +34,7 @@ export default function Page() {
             height={40}
             className="transition-transform duration-300 ease-in-out hover:scale-110"
           />
-        </a>
+        </Link>
       </div>
 
       {/*  quote box  */}
