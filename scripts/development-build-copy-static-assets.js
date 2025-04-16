8d2d8165334a1efcbf704e5f7d9f4f388b3d7abb
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-04-16 13:16:13 -0400

    Add helpful log messages

--- scripts/development-build-copy-static-assets.js
+++ scripts/development-build-copy-static-assets.js
@@ -3,3 +3,5 @@ const fs = require("node:fs");
 
 fs.cpSync("public", ".next/standalone/public", { recursive: true });
 fs.cpSync(".next/static", ".next/standalone/.next/static", { recursive: true });
+
+console.log("Static assets are ready for development build.");
--- scripts/production-build-copy-public.js
+++ scripts/production-build-copy-public.js
@@ -6,3 +6,5 @@ fs.cpSync("public", ".next/standalone/public", { recursive: true });
 fs.mkdirSync("cdn-build", { recursive: true });
 fs.cpSync("public", "cdn-build", { recursive: true });
 fs.cpSync(".next/static", "cdn-build/_next/static", { recursive: true });
+
+console.log("Static assets are ready for the CDN.");
