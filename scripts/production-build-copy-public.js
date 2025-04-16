aef63eb55126fafc5efd0958669eff8a4297a8ac
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-04-15 23:48:10 -0400

    Move to Netlify

--- scripts/production-build-copy-public.js
+++ scripts/production-build-copy-public.js
@@ -2,3 +2,7 @@
 const fs = require("node:fs");
 
 fs.cpSync("public", ".next/standalone/public", { recursive: true });
+
+fs.mkdirSync("cdn-build", { recursive: true });
+fs.cpSync("public", "cdn-build", { recursive: true });
+fs.cpSync(".next/static", "cdn-build/_next/static", { recursive: true });
