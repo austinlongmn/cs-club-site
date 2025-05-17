8075bc27b495d08b5a9e45420e21a4629efd7539
Dylan Weakly <dcweakly@students.unwsp.edu>
2025-05-17 16:04:53 -0500

    Update Students Page

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import React from "react";
 import Link from "next/link";
+import CSCLogo from "@/svg/csc-logo.svg";
+import Image from "@/components/image";
 
 export const metadata: Metadata = {
   title: "Students - UNW CS Club",
@@ -23,18 +25,29 @@ const students = [
 
 const StudentsPage: React.FC = () => {
   return (
-    <div className="ml-5 md:ml-0">
-      <h1 className="pt-10 text-5xl font-extrabold">Students List</h1>
-      <ul className="pt-5 text-xl text-gray-300">
-        {students.map((student) => (
-          <li
-            className="mt-3 transition-colors duration-500 ease-in-out hover:font-bold hover:text-white"
-            key={student.id}
-          >
-            <Link href={`/students/${student.id}`}>{student.name}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col items-center justify-between md:flex-row md:items-start">
+      <div>
+        <h1 className="pt-10 text-5xl font-bold">Our Students</h1>
+        <ul className="pt-5 text-xl text-gray-300">
+          {students.map((student) => (
+            <li
+              className="mt-3 transition-colors duration-500 ease-in-out hover:font-bold hover:text-white"
+              key={student.id}
+            >
+              <Link href={`/students/${student.id}`}>{student.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <Image
+          src={CSCLogo}
+          alt="Computer Science Club Logo"
+          className="w-100 pt-20"
+          width={300}
+          height={100}
+        />
+      </div>
     </div>
   );
 };
