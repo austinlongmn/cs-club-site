f2344c9466ece02ac223f8afff56d3b244eb02af
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-08-15 21:45:33 -0500

    Add example frame

--- src/app/students/austin-long/frame.tsx
+++ src/app/students/austin-long/frame.tsx
@@ -3,29 +3,76 @@ import { Frame, Element } from "@/components/slideshow";
 import Image from "next/image";
 
 import ProfileImage from "@public/images/austin/headshot.jpeg";
+import SwiftLogo from "@/svg/swift-logo-color.svg";
+import RustLogo from "@public/images/general/rust-logo-128x128.png";
+import TypeScriptLogo from "@/svg/ts-logo-128.svg";
+import PythonLogo from "@/svg/python-logo.svg";
+import QRCode from "@public/images/austin/website-qr-code.svg";
 import { Bio } from "./page";
 
 export default function SlideshowFrame() {
   return (
-    <Frame>
-      <div className="flex flex-col gap-5 p-5">
-        <div className="flex flex-row gap-5">
-          <Element animationDirection="up">
-            <div className="font-jetbrains-mono flex items-center justify-center rounded-lg bg-zinc-700 p-10 text-7xl">
-              <div>Austin Long</div>
+    <Frame backgroundColor="#050809">
+      <div className="grid h-full grid-cols-2 grid-rows-[auto_1fr] gap-5 p-5">
+        <Element animationDirection="up">
+          <div className="flex flex-row gap-5">
+            <div className="font-jetbrains-mono flex flex-1 grow items-center justify-center rounded-lg bg-zinc-800 p-10 text-7xl">
+              <div className="text-center">Austin Long</div>
             </div>
-          </Element>
-          <Element animationDirection="up">
             <Image
               src={ProfileImage}
-              className="w-50 rounded-lg"
+              className="w-100 flex-1 rounded-lg"
               alt="Austin Long"
             />
-          </Element>
-        </div>
+          </div>
+        </Element>
+        <Element animationDirection="right">
+          <div className="flex h-full flex-col rounded-lg bg-zinc-800 p-5">
+            <h2 className="font-jetbrains-mono text-center text-4xl">
+              Favorite Programming Languages
+            </h2>
+            <ul className="font-jetbrains-mono flex grow flex-col justify-around text-3xl *:flex *:flex-row *:items-center *:gap-5">
+              <li>
+                <Image alt="Swift Logo" className="w-10" src={SwiftLogo} />
+                <div>Swift</div>
+              </li>
+              <li>
+                <Image alt="Rust Logo" className="w-10" src={RustLogo} />
+                <div>Rust</div>
+              </li>
+              <li>
+                <Image
+                  alt="TypeScript Logo"
+                  className="w-10"
+                  src={TypeScriptLogo}
+                />
+                <div>TypeScript</div>
+              </li>
+              <li>
+                <Image alt="Python Logo" className="w-10" src={PythonLogo} />
+                <div>Python</div>
+              </li>
+            </ul>
+          </div>
+        </Element>
         <Element animationDirection="left">
-          <div className="prose prose-p:text-3xl prose-a:no-underline prose-a:text-foreground prose-p:text-foreground rounded-lg bg-zinc-800 p-5">
+          <div className="prose prose-p:text-3xl prose-a:no-underline prose-a:text-foreground prose-p:text-foreground h-full w-full max-w-none rounded-lg bg-zinc-800 p-5">
             <Bio />
+          </div>
+        </Element>
+        <Element animationDirection="down">
+          <div className="flex h-full flex-col items-center justify-center gap-5 rounded-lg p-5">
+            <h2 className="font-jetbrains-mono text-center text-5xl">
+              Website
+            </h2>
+            <p className="font-jetbrains-mono bold text-center text-4xl">
+              https://austinlong.dev/
+            </p>
+            <Image
+              alt="QR Code to my website"
+              className="w-100 rounded-lg bg-white"
+              src={QRCode}
+            />
           </div>
         </Element>
       </div>
--- src/components/slideshow/slideshow-frame.tsx
+++ src/components/slideshow/slideshow-frame.tsx
@@ -5,9 +5,10 @@ import { FrameIndexContext, SlideshowContext } from "./slideshow";
 
 export interface FrameProps {
   children: React.ReactNode;
+  backgroundColor?: string;
 }
 
-export function Frame({ children }: FrameProps) {
+export function Frame({ children, backgroundColor }: FrameProps) {
   const slideshowContext = useContext(SlideshowContext);
   const animIn = useContext(FrameIndexContext);
   const transition = {
@@ -18,11 +19,17 @@ export function Frame({ children }: FrameProps) {
   const initialScale = slideshowContext.animationScale;
   const targetScale = animIn ? 1 : initialScale;
 
+  const initialBackgroundColor = "rgba(0, 0, 0, 0)";
+  const targetBackgroundColor = animIn
+    ? (backgroundColor ?? initialBackgroundColor)
+    : initialBackgroundColor;
+
+  console.log(targetBackgroundColor);
   return (
     <motion.div
-      initial={{ scale: initialScale }}
-      animate={{ scale: targetScale }}
-      transition={{ scale: transition }}
+      initial={{ scale: initialScale, backgroundColor: initialBackgroundColor }}
+      animate={{ scale: targetScale, backgroundColor: targetBackgroundColor }}
+      transition={{ scale: transition, backgroundColor: transition }}
       className="absolute h-full w-full"
     >
       {children}
